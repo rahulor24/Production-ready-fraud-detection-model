eb4dd424dc1e734421eaeab54ec5905e7640bedc
Rahul <rahuldux@gmail.com>
2026-01-17 17:19:02 +0530

    structural changes

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ from preprocessor import Preprocessor
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("D:/Projects/Fraud Detection/data/raw/fraud_transactions.csv")
-X = df.drop('fraud_flag', axis=1)
-y = df['fraud_flag']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-def RandForest():
+def RandForest(df):
+    X = df.drop('fraud_flag', axis=1)
+    y = df['fraud_flag']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
     rf = RandomForestClassifier(n_estimators=200, min_samples_split=5, min_samples_leaf=4, max_depth=10,
                                 random_state=42, class_weight='balanced', n_jobs=-1, oob_score=True)
 
--- src/st.py
+++ src/st.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import pandas as pd
+import openpyxl
+from main import RandForest
 
 st.title("Fraud Detection")
 
-df = st.file_uploader("Please upload your file")
+file = st.file_uploader("Please upload your file", type= 'xlsx')
+if file:
+    df = pd.read_excel(file)
+    st.dataframe(df)
+
+    train_f1, test_f1, cm = RandForest(df)
+    
+    st.write("Train F1:", train_f1)
+    st.write("\nTest F1:", test_f1)
+    st.write("\nConfusion Matrix:\n", cm)
 
