5b68a85a77271661dffc1554b743765817c81aa4
Rahul <rahuldux@gmail.com>
2026-01-18 21:10:31 +0530

    finalized UI with additional features

--- src/main.py
+++ src/main.py
@@ -1,36 +1,46 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
 from sklearn.pipeline import Pipeline
 from preprocessor import Preprocessor
 
 # load and split data
 
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def RandForest(df):
-    X = df.drop('fraud_flag', axis=1)
-    y = df['fraud_flag']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+class Classifier:
+    def data(df):
+        if 'fraud_flag' in df.columns:
+            X = df.drop('fraud_flag', axis=1)
+            y = df['fraud_flag']
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+            return X_train, X_test, y_train, y_test
+        else:
+            X_test = df
+        return X_test
 
-    rf = RandomForestClassifier(n_estimators=200, min_samples_split=5, min_samples_leaf=4, max_depth=10,
-                                random_state=42, class_weight='balanced', n_jobs=-1, oob_score=True)
+    def RandForest(X_train, y_train):
+        rf = RandomForestClassifier(n_estimators=200, min_samples_split=5, min_samples_leaf=4, max_depth=10,
+                                    random_state=42, class_weight='balanced', n_jobs=-1, oob_score=True)
 
-    rfp = Pipeline([
-        ('preprocessor', Preprocessor()),
-        ('model', rf)
-    ])
+        rfp = Pipeline([
+            ('preprocessor', Preprocessor()),
+            ('model', rf)
+        ])
 
-    rfp.fit(X_train, y_train)
+        rfp.fit(X_train, y_train)
+        return rf, rfp
 
-    y_pred = rfp.predict(X_train)
-    train_f1_score = f1_score(y_train, y_pred)
+    def prediction(model, X):
+        y_prob = model.predict_proba(X)[:,1]
+        y_pred = (y_prob >= 0.36).astype(int)     # best threshold = 0.36
+        return y_pred, y_prob
 
-    y_prob = rfp.predict_proba(X_test)[:,1]
-    y_pred = (y_prob >= 0.36).astype(int)     # best threshold = 0.36
-    test_f1_score = f1_score(y_test, y_pred)
-
-    cm = confusion_matrix(y_test,y_pred)
-
-    return train_f1_score, test_f1_score, cm, rf, X_train, X_test
+    def metrics(Y, y_pred, y_prob):
+        acc = accuracy_score(Y, y_pred)
+        f1 = f1_score(Y, y_pred)
+        pr = precision_score(Y, y_pred)
+        rec =recall_score(Y, y_pred)
+        roc = roc_auc_score(Y,y_prob)
+        cm = confusion_matrix(Y,y_pred)
+        return acc, f1, pr, rec, roc, cm
--- src/st.py
+++ src/st.py
@@ -1,25 +1,56 @@
 import streamlit as st
 import pandas as pd
-from main import RandForest
+from main import Classifier
 import matplotlib.pyplot as plt
 from shap_rf import run_shap_random_forest
 
 st.title("Fraud Detection")
+tab1, tab2 = st.tabs(["Model Overview", "Live Prediction"])
 
-file = st.file_uploader("Please upload your file", type='csv')
-if file:
-    df = pd.read_csv(file)
+with tab1:
+    st.header("Training Dataset")
+    df = pd.read_csv("../data/raw/fraud_transactions.csv")
     st.dataframe(df)
 
-    train_f1, test_f1, cm, rf, X_train, X_test = RandForest(df)
+    st.download_button(
+        label="Download Training Dataset as CSV",
+        data=df.to_csv(index=False).encode('utf-8'),
+        file_name='fraud_transactions.csv',
+        mime='text/csv',
+    )
 
-    st.write("Train F1:", train_f1)
-    st.write("\nTest F1:", test_f1)
-    st.write("\nConfusion Matrix:\n", cm)
+    X_train, X_test, y_train, y_test = Classifier.data(df)
+    rf, rfp = Classifier.RandForest(X_train, y_train)
+
+    y_pred_train, y_prob_train = Classifier.prediction(model=rfp, X=X_train)
+    y_pred_test, y_prob_test = Classifier.prediction(model=rfp, X=X_test)
+    
+    train_accuracy, train_f1, train_prec, train_rec, train_roc, train_cm = Classifier.metrics(y_train, y_pred_train, y_pred_train)
+    test_accuracy, test_f1, test_prec, test_rec, test_roc, test_cm = Classifier.metrics(y_test, y_pred_test, y_prob_test)
+
+    st.header("Important Metrics")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Training Metrics")
+        st.metric(label="Train Accuracy", value=f"{train_accuracy*100:.2f}%")
+        st.metric(label="Train F1", value=f"{train_f1*100:.2f}%")
+        st.metric(label="Train Precision", value=f"{train_prec*100:.2f}%")
+        st.metric(label="Train Recall", value=f"{train_rec*100:.2f}%")
+        st.metric(label="Train ROC-AUC", value=f"{train_roc*100:.2f}%")
+        st.write("Confusion Matrix:\n", train_cm)
+    
+    with col2:
+        st.subheader("Testing Metrics")
+        st.metric(label="Test Accuracy:",value= f"{test_accuracy*100:.2f}%")
+        st.metric(label="Test F1:",value= f"{test_f1*100:.2f}%")
+        st.metric(label="Test Precision:",value= f"{test_prec*100:.2f}%")
+        st.metric(label="Test Recall:",value= f"{test_rec*100:.2f}%")
+        st.metric(label="Test ROC-AUC:",value= f"{test_roc*100:.2f}%")
+        st.write("Confusion Matrix:\n", test_cm)
 
 
-    st.header("Random Forest SHAP Explainability")
-    dataset_choice = st.radio("Pick the dataset on which you want to run shap:\n",["Training Dataset", "Testing Dataset"])
+    st.header("SHAP Explainability")
+    dataset_choice = st.radio("Pick the dataset on which you want to run shap analysis:\n",["Training Dataset", "Testing Dataset"])
     X_input = X_train if dataset_choice == "Training Dataset" else X_test
 
     n_display = st.slider("Number of best features to display:",5,20,10)
@@ -40,4 +71,31 @@ if file:
         st.plotly_chart(fig_beeswarm, use_container_width=True)
 
         st.success("SHAP analysis completed")
+
+with tab2:
+    data = st.file_uploader("Please upload your file for prediction", type='csv')
+    if data:
+        live_df = pd.read_csv(data)
+        st.subheader("Provided Live Transactions")
+        st.dataframe(live_df)
+
+        X_test = Classifier.data(live_df)
+        fraud_flag = Classifier.prediction(model= rfp, X=X_test)
+        
+        st.subheader("Live Predicted Fraud Transactions")
+        live_df['fraud_flag'] = fraud_flag[0]
+        st.dataframe(live_df)
+        st.write("""Note, In fraud_flag column,
+- '0' represents legitimate transaction
+- '1' represents fraud transaction""")
+
+        st.download_button(
+        label="Download Predicted File as CSV",
+        data=live_df.to_csv(index=False).encode('utf-8'),
+        file_name='live_predicted_fraud_transactions.csv',
+        mime='text/csv',
+        )
+
+        st.success("Prediction Successful")
+
 
